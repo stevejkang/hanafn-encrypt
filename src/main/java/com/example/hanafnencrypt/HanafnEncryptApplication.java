b93177a646517f7939c42a1888a73a5976b2babd
stevejkang <iam@juneyoung.io>
2022-06-23 15:12:46 +0900

    Initialize spring

---
+++ src/main/java/com/example/hanafnencrypt/HanafnEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanafnencrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanafnEncryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanafnEncryptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hanafnencrypt/HanafnEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanafnencrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanafnEncryptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
